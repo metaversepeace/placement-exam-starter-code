bf52d4878e2fa9cac1c97ca380cb03529e5fb5c8
metaversepeace <jamesrbroucek@gmail.com>
2021-09-29 21:31:45 -0700

    Add my file

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,65 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+
+let yourName = "James Broucek"; 
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Ginger bread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let gb = 0      // Ginger bread;
+let cc = 0      // Chocolate Chip;
+let sugar = 0   // Sugar Sprinkle;
 
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+document.getElementById('credit').textContent = `Created by ${yourName}`;
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
+
+let gbOrder = 0;
+
+let gbTotalCell = document.querySelector('#qty-gb');
+
+let cookieTotalCell = document.querySelector('#qty-total');
+
+
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+    gbOrder = gbOrder + 1; console.log(gbOrder);
+    gbTotalCell.textContent = gbOrder;
+    cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if(gbOrder > 0){gbOrder = gbOrder - 1}; console.log(gbOrder);
+    gbTotalCell.textContent = gbOrder;
+    cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
+
+
+let ccOrder = 0;
+
+let ccTotalCell = document.querySelector('#qty-cc');
+
+document.getElementById('add-cc').addEventListener('click', function() {
+    ccOrder = ccOrder + 1; console.log(ccOrder);
+    ccTotalCell.textContent = ccOrder;
+    cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
+
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    if(ccOrder > 0){ccOrder = ccOrder - 1}; console.log(ccOrder);
+    ccTotalCell.textContent = ccOrder;
+    cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
+
+
+
+let sugarOrder = 0;
+
+let sugarTotalCell = document.querySelector('#qty-sugar');
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+    sugarOrder = sugarOrder + 1; console.log(sugarOrder);
+sugarTotalCell.textContent = sugarOrder;
+cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
+
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    if(sugarOrder > 0){sugarOrder = sugarOrder - 1}; console.log(sugarOrder);
+    sugarTotalCell.textContent = sugarOrder;
+    cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
